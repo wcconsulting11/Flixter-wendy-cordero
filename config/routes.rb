1643916b2e8de8489bbb862a2e9a33c1320e70cc
Wendy Cordero <wcconsulting11@gmail.com>
2019-10-01 03:29:24 +0000

    add course error

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 	devise_for :users
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'static_pages#index'
+	resources :courses, only: [:index, :show]
+	namespace :instructor do
+		 resources :courses, only: [:new, :ceate, :show] do
+			resources :sections, only: [:name, :create]
+	    end
+    end
 end
---
+++ test/controllers/instructor/section_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Instructor::SectionControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
