98417e92ecec3f0bfa1781960064e9125d70bc56
Wendy Cordero <wcconsulting11@gmail.com>
2019-10-23 23:33:37 +0000

    Working course submission by Jason

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     mount_uploader :image, ImageUploader
     belongs_to :user
-    belongs_to :instructor, class_name: 'User'
+    belongs_to :instructor, class_name: 'User', foreign_key: :user_id, optional: true
     has_many :sections
     has_many :enrollments
     
